0a439701a3c7e0189059c1ee629814fd178f235b
Miguel-Luis-1 <114661451+Miguel-Luis-1@users.noreply.github.com>
2023-04-13 10:02:35 -0300

    Add files via upload

---
+++ TropicalGarden2/JS/promocoes.js
@@ -0,0 +1,5 @@
+const filterSelect = document.getElementById("filter-select");
+filterSelect.addEventListener("change", (event) => {
+  const selectedValue = event.target.value;
+  // Use o valor selecionado para filtrar as flores no seu e-commerce
+});
